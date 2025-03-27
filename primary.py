d881563c423e46319cd5fe72b045c2d50d39254f
Pratyush K <kpratyush1997@gmail.com>
2025-03-27 16:10:00 +1100

    Built agent status tracker

--- primary.py
+++ primary.py
@@ -12,14 +12,25 @@ rng = np.random.default_rng(seed = 42)
 
 #####
 
+def create_status_vars(PopSize, GrpSizes, NumGrps):
+    ID = np.empty((NumGrps, np.max(GrpSizes)))
+    Status = np.empty((NumGrps, np.max(GrpSizes)))
+    ID[:]=np.nan
+    Status[:]=np.nan
+    for ng in range(NumGrps):
+        ID[ng, 0:GrpSizes[ng]] = np.arange(0, GrpSizes[ng], 1)
+        Status[ng, 0:GrpSizes[ng]] = 0
+    return Status   
 
-def init_state_vars(NumGrps, GrpSizes):
+def init_vars(PopSize, NumGrps, GrpSizes): #Initialises with one infection in each group
     S = GrpSizes-1 #Needs to be an array containing number of people in each S compartment
     Ia = np.ones(NumGrps)
     Ib = np.zeros(NumGrps)
     Ra = np.zeros(NumGrps)
     Rb = np.zeros(NumGrps)
-    return S, Ia, Ib, Ra, Rb    
+    Status = create_status_vars(PopSize, GrpSizes, NumGrps)
+    Status[:, 0] = 1 #Assign Infected (a) status for the first agent of each group
+    return S, Ia, Ib, Ra, Rb, Status    
 
 # Computing Rates
 def compute_rates_probs(beta, gamma, mu, PopSize, S, Ia, Ib, Ra, Rb, ContactMatrix):
@@ -35,23 +46,53 @@ def compute_rates_probs(beta, gamma, mu, PopSize, S, Ia, Ib, Ra, Rb, ContactMatr
     return RateSum, AllPs, AllRates
 
 ## A function that takes Event as input and does the operation on state variables
-def step(Event, S, Ia, Ib, Ra, Rb, NumGrps):
+def step(Event, S, Ia, Ib, Ra, Rb, Status, GrpSizes, NumGrps, rnd):
     if Event<NumGrps:
         S[Event]-=1
         Ia[Event]+=1
+        #Select random susceptible
+        GrpNum = Event
+        GrpSize = GrpSizes[GrpNum]
+        Agents = np.where(Status[GrpNum] == 0)[0]
+        SelectedAgent = Agents[int(rnd*len(Agents))]
+        Status[GrpNum, SelectedAgent] = 1
     elif Event<2*NumGrps:
         Ia[Event - NumGrps]-=1
         Ib[Event - NumGrps]+=1
+        #Select random Infected (a)
+        GrpNum = Event - NumGrps
+        GrpSize = GrpSizes[GrpNum]
+        Agents = np.where(Status[GrpNum] == 1)[0]
+        SelectedAgent = Agents[int(rnd*len(Agents))]
+        Status[GrpNum, SelectedAgent] = 2
     elif Event<3*NumGrps:
         Ib[Event - 2*NumGrps]-=1
         Ra[Event - 2*NumGrps]+=1
+        #Select random Infected (b)
+        GrpNum = Event - 2*NumGrps
+        GrpSize = GrpSizes[GrpNum]
+        Agents = np.where(Status[GrpNum] == 2)[0]
+        SelectedAgent = Agents[int(rnd*len(Agents))]
+        Status[GrpNum, SelectedAgent] = 3
     elif Event<4*NumGrps:
         Ra[Event - 3*NumGrps]-=1
         Rb[Event - 3*NumGrps]+=1
+        #Select random Recovered (a)
+        GrpNum = Event - 3*NumGrps
+        GrpSize = GrpSizes[GrpNum]
+        Agents = np.where(Status[GrpNum] == 3)[0]
+        SelectedAgent = Agents[int(rnd*len(Agents))]
+        Status[GrpNum, SelectedAgent] = 4
     elif Event<5*NumGrps:
         Rb[Event - 4*NumGrps]-=1
         S[Event - 4*NumGrps]+=1
-    return S, Ia, Ib, Ra, Rb
+        #Select random Recovered (b)
+        GrpNum = Event - 4*NumGrps
+        GrpSize = GrpSizes[GrpNum]
+        Agents = np.where(Status[GrpNum] == 4)[0]
+        SelectedAgent = Agents[int(rnd*len(Agents))]
+        Status[GrpNum, SelectedAgent] = 0
+    return S, Ia, Ib, Ra, Rb, Status
 
 
 def simulate(beta, InfD, ImmD, NumSteps, TargetPopSize):
@@ -65,8 +106,8 @@ def simulate(beta, InfD, ImmD, NumSteps, TargetPopSize):
                             int(0.1*TargetPopSize)])
     NumGrps = len(GrpSizes)
     PopSize = np.sum(GrpSizes)
-    #ContactMatrix = rng.random((NumGrps, NumGrps))
     
+    # The matrix below was randomly generated.
     # ContactMatrix = np.array([[0.72072839, 0.71123776, 0.20269503, 0.0366554 , 0.30379952],
     #        [0.1571363 , 0.39578848, 0.97934612, 0.18107137, 0.31887394],
     #        [0.72511432, 0.50918278, 0.04392814, 0.16169002, 0.93524955],
@@ -77,29 +118,33 @@ def simulate(beta, InfD, ImmD, NumSteps, TargetPopSize):
     
     State = np.zeros(TargetPopSize)
     Events = np.arange(0, 5*NumGrps, 1) # 5 because there are five states, S, Ia, Ib, Ra, Rb
-    S, Ia, Ib, Ra, Rb = init_state_vars(NumGrps, GrpSizes)
+    S, Ia, Ib, Ra, Rb, Status = init_vars(PopSize, NumGrps, GrpSizes)
     ts = np.zeros(NumSteps)
     Ss, Ias, Ibs, Ras, Rbs = np.zeros((NumSteps, NumGrps)), np.zeros((NumSteps, NumGrps)), np.zeros((NumSteps, NumGrps)), np.zeros((NumSteps, NumGrps)), np.zeros((NumSteps, NumGrps))
     Ss[0, :], Ias[0, :], Ibs[0, :], Ras[0, :], Rbs[0, :] = S, Ia, Ib, Ra, Rb
+    
+    #Pre-generated random numbers
+    rnds = rng.uniform(0, 1, NumSteps)
     for counter in range(1, NumSteps):
         if np.any(S<0) or np.any(Ia<0) or np.any(Ib<0) or np.any(Ra<0) or np.any(Rb<0):
             print('Error, negative values')
             break
         elif np.sum(Ia + Ib)!=0:
             RateSum, AllPs, AllRates = compute_rates_probs(beta, gamma, mu, PopSize, S, Ia, Ib, Ra, Rb, ContactMatrix)
-            Event = rng.choice(Events, size = 1, p = AllPs)
+            Event = rng.choice(Events, size = 1, p = AllPs)[0]
             EventTimeStep = rng.exponential(scale = 1/RateSum)
             ts[counter] = ts[counter-1]+EventTimeStep
-            S, Ia, Ib, Ra, Rb = step(Event, S, Ia, Ib, Ra, Rb, NumGrps)
+            S, Ia, Ib, Ra, Rb, Status = step(Event, S, Ia, Ib, Ra, Rb, Status, GrpSizes, NumGrps, rnds[counter])
+
         Ss[counter, :], Ias[counter, :], Ibs[counter, :], Ras[counter, :], Rbs[counter, :] = S, Ia, Ib, Ra, Rb
     
-    return ts, Ss, Ias, Ibs, Ras, Rbs
+    return ts, Ss, Ias, Ibs, Ras, Rbs, Status, PopSize
 
 def calibrate(beta, InfD, ImmD):
     NumSteps = 50000
-    PopSize = 1000
-    ts, Ss, Ias, Ibs, Ras, Rbs = simulate(beta, InfD, ImmD, NumSteps, PopSize)
-    return ts, Ss/PopSize, Ias/PopSize, Ibs/PopSize, Ras/PopSize, Rbs/PopSize
+    TargetPopSize = 1000
+    ts, Ss, Ias, Ibs, Ras, Rbs, Status, PopSize = simulate(beta, InfD, ImmD, NumSteps, TargetPopSize)
+    return ts, Ss/PopSize, Ias/PopSize, Ibs/PopSize, Ras/PopSize, Rbs/PopSize, Status
 
 def sampler(SamplingTimes, ts, xs):
     Sample = np.empty(SamplingTimes.shape)
@@ -114,12 +159,12 @@ def sampler(SamplingTimes, ts, xs):
 ''' Scheme = SIIRRS '''
 
 # Input params
-beta = 2/7 # in 1/days
+beta = 5/7 # in 1/days
 InfD = 2*7 # in days
 ImmD = 1*7 # in days
 
 
-ts, ss, ias, ibs, ras, rbs = calibrate(beta, InfD, ImmD)
+ts, ss, ias, ibs, ras, rbs, status = calibrate(beta, InfD, ImmD)
 plt.plot(ts, np.sum(ss, 1), label = 'Susceptible')
 plt.plot(ts, (np.sum(ias, 1)), label = 'Infected (a)')
 plt.plot(ts, (np.sum(ibs, 1)), label = 'Infected (b)')
